b8b1d1e3e183edc75486dee81ed8b266b1b62477
Tauseef Ahmad <ertauseefahmad@gmail.com>
2025-11-01 11:00:12 +0530

    1novjs

---
+++ javascript/day1/script.js
@@ -0,0 +1,26 @@
+// let a=10;
+// let b=3;
+// console.log(a+b);
+
+// let x = 5;
+//  x = x + 3;
+
+//  if (a > 5 && a < 20 && a === 10) {
+//     console.log("Saari conditions sahi hain ✅");
+// } else {
+//     console.log("Koi condition galat hai ❌");
+// }
+// test()
+// function test() { console.log("Hello") }
+
+// hello()
+// var hello = function() { console.log("Hi") }
+
+
+// let age= prompt("enter your age");
+
+// if(age>18){
+//     console.log("Adult");
+// } else{
+//     console.log("Minor");
+// }
---
+++ javascript/day2/script.js
@@ -0,0 +1,6 @@
+// How do you convert a string to a number in JavaScript?
+// let str = "123";
+// let num = Number(str);
+// console.log(num)
+// if(0 == "0") console.log("Yes");
+if(0 === "0") console.log("No");
