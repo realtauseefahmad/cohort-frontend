1b0f15b97e8caec1b1791e8c4bea8e8073e39636
Tauseef Ahmad <ertauseefahmad@gmail.com>
2025-12-19 00:54:18 +0530

    class1

---
+++ react/class1/src/App.jsx
@@ -0,0 +1,37 @@
+function App() {
+
+  return (
+    <>
+      <nav>
+        <div className="left">
+          <a href="#">About Me</a>
+          <a href="#">Portfolio</a>
+          <a href="#">Services</a>
+          <a href="#">Blog</a>
+        </div>
+        <div className="right">
+          <a href="#">BOOK A CALL</a>
+        </div>
+      </nav>
+      <header>
+        <img src="./src/assets/img.png"/>
+        <div className="top">
+          <div className="details">
+            <h2>+200</h2>
+            <p>project complete</p>
+          </div>
+          <div className="details">
+            <h2>+50</h2>
+            <p>Startup Raise</p>
+          </div>
+        </div>
+        <div className="buttom">
+          <h1>Hello</h1>
+          <p>- its D.Nova a Design wizerd</p>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App
